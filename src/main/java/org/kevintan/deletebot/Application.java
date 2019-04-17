c19256203ae08a27934558f129d597e8765c886f
Kevin Tan <kevin.tan97@hotmail.com>
2019-04-16 21:35:05 -0400

    Initial commit to DeleteBot using spring

---
+++ src/main/java/org/kevintan/deletebot/Application.java
@@ -0,0 +1,17 @@
+package org.kevintan.deletebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kevin Tan 2019-04-16
+ */
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
